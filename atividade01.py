735186b07d52753158f4bb467185e2936557aaa5
kellton11 <kelltonaraujo11@gmail.com>
2024-09-04 20:31:56 -0300

    soma de dois números

---
+++ atividade01.py
@@ -0,0 +1,7 @@
+# Crie um programa que receba dois números do usuário e exiba a soma desses números.
+n1 = int(input("digite um numero "))
+n2 = int(input("digite o segundo numero "))
+
+soma = n1 + n2
+
+print(f"a soma do valor é = {soma}")
